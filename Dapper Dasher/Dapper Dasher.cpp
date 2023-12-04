ff8329f94a6ce03adfe873afa7160ba4f93f27e0
Praveen <rpraveen.engineer@gmail.com>
2023-12-04 23:03:32 +0530

    Added one more nebula

--- Dapper Dasher/Dapper Dasher.cpp
+++ Dapper Dasher/Dapper Dasher.cpp
@@ -8,9 +8,9 @@ int main()
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dapper Dasher");
 
     int velocity = 0;
-    const int jump_vel = 600;
+    const int jump_vel = 700;
 
-    const int GRAVITY = 1800;
+    const int GRAVITY = 1600;
     bool can_jump = true;
 
     // scarfy
@@ -35,7 +35,16 @@ int main()
         static_cast<float>(nebula.height / 8) }; // x, y, width, height
 
     Vector2 nebula_pos = { WINDOW_WIDTH, WINDOW_HEIGHT - nebula_rect.height };
-    int nebula_vel = 600;
+    int nebula_vel = 200;
+    int nebula_frame = 0;
+    float nebula_running_time = 0.0f;
+
+    Rectangle nebula_rect_2 = { 0.0, 0.0, static_cast<float>(nebula.width / 8),
+        static_cast<float>(nebula.height / 8) };
+    Vector2 nebula_pos_2 = { WINDOW_WIDTH + 200, WINDOW_HEIGHT - nebula_rect_2.height };
+    int nebula_frame_2 = 0;
+    float nebula_running_time_2 = 0.0f;
+
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -80,14 +89,38 @@ int main()
                 }
             }
         }
-       
 
         scarfy_rect.x = frame * scarfy_rect.width;
-
+       
         nebula_pos.x -= nebula_vel * dt;
+        nebula_running_time += dt;
+        if (nebula_running_time > update_time)
+        {
+            nebula_frame++;
+            if (nebula_frame > 7) {
+                nebula_frame = 0;
+            }
+            nebula_running_time = 0.0f;
+        }
+        nebula_rect.x = nebula_frame * nebula_rect.width;
+
+        nebula_pos_2.x -= nebula_vel * dt;
+        nebula_running_time_2 += dt;
+        if (nebula_running_time_2 > update_time)
+        {
+            nebula_frame_2++;
+            if (nebula_frame_2 > 7)
+            {
+                nebula_frame_2 = 0;
+            }
+            nebula_running_time_2 = 0.0f;
+        }
+        nebula_rect_2.x = nebula_frame_2 * nebula_rect_2.width;
+        
 
         DrawTextureRec(scarfy, scarfy_rect, scarfy_pos, WHITE);
         DrawTextureRec(nebula, nebula_rect, nebula_pos, WHITE);
+        DrawTextureRec(nebula, nebula_rect_2, nebula_pos_2, RED);
 
         EndDrawing();
     }
