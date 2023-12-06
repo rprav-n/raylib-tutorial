2f9a4cd9a54bbeab9d60c62dedb05cca09e41a07
Praveen <rpraveen.engineer@gmail.com>
2023-12-06 23:46:13 +0530

    setup

---
+++ ClassyClash/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include "raylib.h"
+
+int main()
+{
+    const int WIN_WIDTH = 384;
+    const int WIN_HEIGHT = 384;
+
+    InitWindow(WIN_WIDTH, WIN_HEIGHT, "Classy Clash");
+
+    Texture2D map = LoadTexture("./nature_tileset/background.png");
+    Vector2 map_pos = {0.0f, 0.0f};
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        DrawTextureEx(map, map_pos, 0.0f, 4.0f, WHITE);
+
+        EndDrawing();
+    }
+
+    UnloadTexture(map);
+    CloseWindow();
+
+    return 0;
+}
