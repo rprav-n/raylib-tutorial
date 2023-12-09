511ea330bb6bb8783836688a6764c32ab0f3164f
Praveen <rpraveen.engineer@gmail.com>
2023-12-09 20:07:05 +0530

    Added movement

--- ClassyClash/main.cpp
+++ ClassyClash/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -10,12 +11,26 @@ int main()
 
     Texture2D map = LoadTexture("./nature_tileset/background.png");
     Vector2 map_pos = {0.0f, 0.0f};
+    const float SCALE = 4.0f;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        Vector2 direction = {};
+        if (IsKeyDown(KEY_A))
+            direction.x = -1;
+        if (IsKeyDown(KEY_D))
+            direction.x = 1;
+        if (IsKeyDown(KEY_W))
+            direction.y = -1;
+        if (IsKeyDown(KEY_S))
+            direction.y = 1;
+
+        direction = Vector2Normalize(direction);
+        map_pos = Vector2Subtract(map_pos, Vector2Scale(direction, SCALE));
 
         DrawTextureEx(map, map_pos, 0.0f, 4.0f, WHITE);
 
