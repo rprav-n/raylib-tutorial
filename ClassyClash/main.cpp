c1b1e19035c97f8e7bb2bd969442ff1f8f87181d
Praveen <rpraveen.engineer@gmail.com>
2023-12-09 20:29:09 +0530

    Animating the character

--- ClassyClash/main.cpp
+++ ClassyClash/main.cpp
@@ -13,17 +13,30 @@ int main()
     Vector2 map_pos = {0.0f, 0.0f};
     const float SCALE = 4.0f;
 
-    Texture2D knight = LoadTexture("./characters/knight_idle_spritesheet.png");
+    Texture2D knight_idle = LoadTexture("./characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("./characters/knight_run_spritesheet.png");
+
+    Texture2D knight = knight_idle;
     Vector2 knight_pos = {
         WIN_WIDTH / 2.0f - SCALE * 0.5f * knight.width / 6.0f,
         WIN_HEIGHT / 2.0f - SCALE * 0.5f * knight.height,
     };
+    // 1: right, -1: left
+    float right_left = {1.f};
+
+    // animation variables
+    const float UPDATE_TIME = 1.f / 12.f;
+    float running_time = 0.f;
+    int frame = 1;
+    const int MAX_FRAMES = 6;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        float dt = GetFrameTime();
 
         Vector2 direction = {};
         if (IsKeyDown(KEY_A))
@@ -35,13 +48,38 @@ int main()
         if (IsKeyDown(KEY_S))
             direction.y = 1;
 
-        direction = Vector2Normalize(direction);
-        map_pos = Vector2Subtract(map_pos, Vector2Scale(direction, SCALE));
+        if (Vector2Length(direction) != 0.0)
+        {
+            knight = knight_run;
+            direction = Vector2Normalize(direction);
+            map_pos = Vector2Subtract(map_pos, Vector2Scale(direction, SCALE));
 
+            if (direction.x != 0)
+            {
+                right_left = direction.x < 0.f ? -1.f : 1.f;
+            }
+        }
+        else
+        {
+            knight = knight_idle;
+        }
+
+        // draw the map
         DrawTextureEx(map, map_pos, 0.0f, 4.0f, WHITE);
-        Rectangle source = Rectangle{0, 0, knight.width / 6.0f, (float)knight.height};
-        Rectangle dest = Rectangle{knight_pos.x, knight_pos.y, SCALE * knight.width / 6.0f, SCALE * knight.height};
 
+        // update knight animation
+        running_time += dt;
+        if (running_time > UPDATE_TIME)
+        {
+            running_time = 0.f;
+            frame++;
+            if (frame > MAX_FRAMES)
+                frame = 0;
+        }
+
+        // draw the knight
+        Rectangle source = Rectangle{frame * knight.width / 6.0f, 0, right_left * knight.width / 6.0f, (float)knight.height};
+        Rectangle dest = Rectangle{knight_pos.x, knight_pos.y, SCALE * knight.width / 6.0f, SCALE * knight.height};
         DrawTexturePro(knight, source, dest, Vector2{0, 0}, 0.0f, WHITE);
 
         EndDrawing();
